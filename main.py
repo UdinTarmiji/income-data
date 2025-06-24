c35b18d9c0ae26be49ef7bf90905b91d396de788
UdinTarmiji <104237379+UdinTarmiji@users.noreply.github.com>
2025-06-24 07:39:52 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+#load dataset
+url ="https://raw.githubusercontent.com/UdinTarmiji/income-data/refs/heads/main/data/income_data.csv"
+data = pd.read_csv(url)
+
+#siapkan data
+x = data[["jam_kerja", "jumlah_klien"]]
+y = data["pendapatan"]
+
+#latih model ai
+model = LinearRegression()
+model.fit(x, y)
+
+#prediksi pendapatan
+jam = 40
+klien = 4
+prediksi = model.predict([[jam, klien]])
+
+
+print(f"Prediksi Pendapatan: Ro {int(prediksi[0]):,}")
